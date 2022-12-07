82a33879b89dd699fa5aaeb260ba43297af13a2c
vasundhra <mishraanjali848@gmail.com>
2022-12-07 10:20:03 +0530

    updated

--- check_prime.js
+++ check_prime.js
@@ -1,13 +1,13 @@
 
 let count=0;
-let number =5;
+let number =1;
 
-for(let i=1; i<number; i++){
-if(number%i==0){
-count++
-}
-if(count==0){
-console.log(number, "this is prime")
-}else{
-console.log(number, "this is not prime")
-}
+for(let i=1; i<=number; i++)
+if(number%i==0)
+count++;
+
+if(count==2)
+console.log(number, " is prime");
+
+else
+console.log(number, " is not prime");
